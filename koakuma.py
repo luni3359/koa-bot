230b2a3467a3e04ab80e60f3c23aea8dd16cd78c
luni3359 <lunibackup@outlook.com>
2019-01-12 18:23:31 -0600

    Use commands extension and change ini to jsonc

--- koakuma.py
+++ koakuma.py
@@ -4,111 +4,162 @@ import random
 import tweepy
 import discord
 import asyncio
-from configparser import ConfigParser
+import datetime
+import commentjson
 from discord.ext import commands
-from discord.ext.commands import Bot
 
 SOURCE_DIR = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(SOURCE_DIR, "config.jsonc")) as json_file:
+    data = commentjson.load(json_file)
 
-config = ConfigParser()
-config.read(os.path.join(SOURCE_DIR, "config.ini"))
-
-auth = tweepy.OAuthHandler(config.get("twitter", "consumer"), config.get("twitter", "consumer_secret"))
-auth.set_access_token(config.get("twitter", "token"), config.get("twitter", "token_secret"))
+auth = tweepy.OAuthHandler(data["keys"]["twitter"]["consumer"], data["keys"]["twitter"]["consumer_secret"])
+auth.set_access_token(data["keys"]["twitter"]["token"], data["keys"]["twitter"]["token_secret"])
 twitter_api = tweepy.API(auth)
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="!")
 
 
-@client.event
+# Get info about an artist based on a previous immediate message containing a valid url from either
+# twitter, danbooru or pixiv.
+@bot.group(aliases=["art"])
+async def artist(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send("I should be fetching artist data!")
+
+
+@artist.command(aliases=["twit"])
+async def twitter(ctx):
+    await ctx.send("Birb.")
+
+
+@artist.command(aliases=["dan"])
+async def danbooru(ctx):
+    await ctx.send("Box.")
+
+
+@artist.command(aliases=["pix"])
+async def pixiv(ctx):
+    await ctx.send("Navi?")
+
+
+# Search on danbooru!
+@bot.command(name="danbooru", aliases=["dan"])
+async def search_danbooru(ctx):
+    await ctx.send("Searching is fun!")
+
+
+@bot.command(aliases=["ava"])
+async def avatar(ctx):
+    embed = discord.Embed()
+    embed.set_image(url=ctx.message.author.avatar_url)
+    embed.set_author(
+        name="{} #{}".format(ctx.message.author.name, ctx.message.author.discriminator),
+        icon_url=ctx.message.author.avatar_url
+    )
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def ci(ctx):
+    await ctx.send(ctx.channel.id)
+
+
+@bot.event
 async def on_ready():
     print("ready!")
+    # Change play status to something fitting
+    await bot.change_presence(activity=discord.Game(name="with books"))
 
 
-@client.event
-async def on_message_delete(message):
-    if message.author.bot:
-        return
+@bot.event
+async def on_message(msg):
+    global activity_count, last_channel_with_activity, activity_warned
 
-    channel = message.channel
-
-    deleted_quotes = [
-        "{} said something. Nobody was there to listen...".format(message.author.mention),
-        "I faintly heard {} whisper something. I wonder what they said...?".format(message.author.mention),
-        "Is that you... {}? I think you need to be heard...".format(message.author.mention),
-        "Mistakes always happen, but remember that not letting it out is dangerous, {}.".format(message.author.mention)
-    ]
-
-    await channel.send(random.choice(deleted_quotes))
-
-
-@client.event
-async def on_message(message):
     # Prevent bot from spamming itself
-    if message.author.bot:
+    if msg.author.bot:
         return
 
     # Test for image urls
-    urls = get_urls(message.content)
-    if len(urls) > 0 and len(message.embeds) > 0:
+    urls = get_urls(msg.content)
+    if len(urls) > 0:
         domains = get_domains(urls)
         for i in range(len(domains)):
             domain = domains[i]
             if "twitter" in domain:
-                await get_twitter_gallery(message, urls[i])
+                await get_twitter_gallery(msg, urls[i])
 
-    # Features under testing, only allowed under this channel
-    if message.channel.name != "koa-bot":
-        # None, currently
-        return
+    if last_channel_with_activity != msg.channel.id or len(urls) > 0:
+        last_channel_with_activity = msg.channel.id
+        activity_count = 0
+
+    activity_count += 1
+
+    if str(msg.channel.id) in data["rules"]["quiet_channels"]:
+        if not activity_warned and activity_count >= data["rules"]["quiet_channels"][str(msg.channel.id)]["max_messages_without_embeds"]:
+            activity_warned = True
+            await msg.channel.send(random.choice(data["quotes"]["quiet_channel_past_threshold"]))
+
+    await bot.process_commands(msg)
 
 
-async def get_twitter_gallery(message, url):
-    channel = message.channel
+async def get_twitter_gallery(msg, url):
+    channel = msg.channel
 
     # Checking whether or not it contains an id
-    id = url.split("/status/")
-    if len(id) == 2:
-        id = id[1].split('?')[0]
-        tweet = twitter_api.get_status(id, tweet_mode="extended")
+    if not "/status/" in url:
+        return
 
-        if not hasattr(tweet, "extended_entities") or len(tweet.extended_entities["media"]) <= 1:
-            print("Preview gallery not applicable.")
+    parsed_id = url.split("/status/")[1].split('?')[0]
+    tweet = twitter_api.get_status(parsed_id, tweet_mode="extended")
+
+    if not hasattr(tweet, "extended_entities") or len(tweet.extended_entities["media"]) <= 1:
+        print("Preview gallery not applicable.")
+        return
+
+    print(msg.embeds)
+    for e in msg.embeds:
+        print(str(datetime.datetime.now()))
+        print(dir(e))
+        print(e.url)
+    if len(msg.embeds) <= 0:
+        print("I wouldn't have worked. Embeds report as 0 on the first try after inactivity on message #{} at {}.".format(msg.id, str(datetime.datetime.now())))
+        # await channel.send("I wouldn't have worked")
+
+    gallery_pics = []
+    for picture in tweet.extended_entities["media"][1:]:
+        if picture["type"] != "photo":
             return
 
-        gallery_pics = []
-        for picture in tweet.extended_entities["media"][1:]:
-            if picture["type"] != "photo":
-                return
+        # Appending :orig to get a better image quality
+        gallery_pics.append(picture["media_url_https"] + ":orig")
 
-            gallery_pics.append(picture["media_url_https"] + ":orig")
+    gallery_pics_total = len(gallery_pics)
+    for picture in gallery_pics:
+        gallery_pics_total -= 1
 
-        gallery_pics_total = len(gallery_pics)
-        for picture in gallery_pics:
-            gallery_pics_total -= 1
+        embed = discord.Embed()
+        embed.set_author(
+            name="{} (@{})".format(tweet.author.name, tweet.author.screen_name),
+            url="https://twitter.com/{}".format(tweet.author.screen_name),
+            icon_url=tweet.author.profile_image_url_https
+        )
+        embed.set_image(url=picture)
 
-            embed = discord.Embed()
-            embed.set_author(
-                name="{} (@{})".format(tweet.author.name, tweet.author.screen_name),
-                url="https://twitter.com/{}".format(tweet.author.screen_name),
-                icon_url=tweet.author.profile_image_url_https
+        # If it's the last picture to show, add a brand footer
+        if gallery_pics_total <= 0:
+            embed.set_footer(
+                text="Twitter",
+                icon_url=data["assets"]["twitter"]["favicon"]
             )
-            embed.set_image(url=picture)
 
-            # If it's the last picture to show, add a brand footer
-            if gallery_pics_total <= 0:
-                embed.set_footer(
-                    text="Twitter", icon_url="https://images-ext-1.discordapp.net/external/bXJWV2Y_F3XSra_kEqIYXAAsI3m1meckfLhYuWzxIfI/https/abs.twimg.com/icons/apple-touch-icon-192x192.png"
-                )
-
-            await channel.send(embed=embed)
+        await channel.send(embed=embed)
 
 
 def get_urls(string):
     # findall() has been used
     # with valid conditions for urls in string
-    url = re.findall("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", string)
-    return url
+    matching_urls = re.findall("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", string)
+    return matching_urls
 
 
 def get_domains(array):
@@ -121,4 +172,7 @@ def get_domains(array):
     return domains
 
 
-client.run(config.get("discord", "token"))
+last_channel_with_activity = None
+activity_count = 0
+activity_warned = False
+bot.run(data["keys"]["discord"]["token"])

e201b191d7a4245e0ebec51f0488ed796e6c89c0
luni3359 <lunibackup@outlook.com>
2019-03-02 17:54:06 -0600

    Add @staticmethod decorator

--- cogs/linkparser.py
+++ cogs/linkparser.py
@@ -3,7 +3,7 @@ import re
 from discord.ext import commands
 
 
-class LinkParserCog(commands.Cog, name="Owner Commands"):
+class LinkParserCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
@@ -16,26 +16,30 @@ class LinkParserCog(commands.Cog, name="Owner Commands"):
         if urls:
             domains = self.get_domains(urls)
             for i, domain in enumerate(domains):
-                if 'twitter.com' in domain:
+                if self.bot.assets['twitter']['domain'] in domain:
                     print('twitter link')
                     # await get_twitter_gallery(msg, urls[i])
 
-                if 'pixiv.net' in domain:
+                if self.bot.assets['pixiv']['domain'] in domain:
                     print('pixiv link')
                     # await get_pixiv_gallery(msg, urls[i])
 
-    def get_urls(self, string):
-        # findall() has been used
-        # with valid conditions for urls in string
-        regex_exp = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'  # pylint: disable=anomalous-backslash-in-string
-        matching_urls = re.findall(regex_exp, string)
+    @staticmethod
+    def get_urls(message: str):
+        """Get all urls from a given string"""
+
+        url_pattern = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+        matching_urls = re.findall(url_pattern, message)
         return matching_urls
 
-    def get_domains(self, array):
+    @staticmethod
+    def get_domains(urls: list):
+        """Get a list of domains from a list of strings
+        https://stackoverflow.com/questions/9626535/get-protocol-host-name-from-url#answer-36609868"""
+
         domains = []
 
-        for url in array:
-            # thanks dude https://stackoverflow.com/questions/9626535/get-protocol-host-name-from-url#answer-36609868
+        for url in urls:
             domain = url.split('//')[-1].split('/')[0].split('?')[0]
             domains.append(domain)
         return domains

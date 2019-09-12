d55c5f27a5251ed3a1a92ab1b5a3551d04b35e76
luni3359 <lunibackup@outlook.com>
2019-09-12 14:51:30 -0500

    Add ssh connectivity checks

--- koaupdate.sh
+++ koaupdate.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # This script automatically sends updates to the bot. In the future it should also restart the running instance.
 
+echo "Updating bot..."
+
 if [ -z ${KOAKUMA_HOME} ]; then
     echo "\$KOAKUMA_HOME is not defined. It needs to point to the bot's directory."
-    exit 1
-fi
-
-if [ -z ${KOAKUMA_CONNSTR} ]; then
-    echo "\$KOAKUMA_CONNSTR is not defined. It needs to point to the bot's hosting machine."
     exit 1
 fi
 
@@ -16,8 +13,28 @@ if [ ! -d ${KOAKUMA_HOME} ]; then
     exit 1
 fi
 
+if [ -z ${KOAKUMA_CONNSTR} ]; then
+    echo "\$KOAKUMA_CONNSTR is not defined. It needs to point to the bot's hosting machine."
+    exit 1
+fi
+
+# redirects STDERR to /dev/null to hide ssh error messages
+REMOTE_HOME=$(ssh ${KOAKUMA_CONNSTR} 'source ~/.profile; echo $KOAKUMA_HOME' 2> /dev/null)
+ssh_return_value=$?
+
+# Test if host is up
+if [ $ssh_return_value -ne 0 ]; then
+    echo "Unable to connect to host (${KOAKUMA_CONNSTR}) [Return value: $ssh_return_value]"
+    exit 1
+fi
+
+if [ -z ${REMOTE_HOME} ]; then
+    echo "The remote \$KOAKUMA_HOME env var is empty or set incorrectly."
+    exit
+fi
+
 # Appending home of the remote koakuma
-TARGET=${KOAKUMA_CONNSTR}:$(ssh ${KOAKUMA_CONNSTR} 'source ~/.profile; echo $KOAKUMA_HOME')
+TARGET=${KOAKUMA_CONNSTR}:${REMOTE_HOME}
 echo "Transferring from ${KOAKUMA_HOME} to ${TARGET}"
 
 rsync -aXv --exclude=.* --exclude=__pycache__ --exclude=venv --include=.python-version --progress ${KOAKUMA_HOME}/ ${TARGET}

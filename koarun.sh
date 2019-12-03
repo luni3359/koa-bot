d8aaec4121ba3896b0d65cf58c2583d992fa205e
luni3359 <lunibackup@outlook.com>
2019-12-02 19:14:38 -0600

    Replace scripts with general-purpose bash script

--- koarun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script is run automatically when the pi runs, directly from /etc/xdg/autostart/koa-bot.desktop
-# env var is set at ~/.profile
-python3 ${KOAKUMA_HOME}/koakuma.py

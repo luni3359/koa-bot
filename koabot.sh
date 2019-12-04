7008a2e75f8e31e356e806a47a9df084192e0f5a
luni3359 <lunibackup@outlook.com>
2019-12-04 17:51:22 -0600

    Add update dependencies option

--- koabot.sh
+++ koabot.sh
@@ -2,10 +2,7 @@
 # This script is run automatically when the pi runs, directly from /etc/xdg/autostart/koa-bot.desktop
 # env var is set at ~/.profile
 
-function update() {
-    # Automatically sends updates to the bot. In the future it should also restart the running instance.
-    echo "Updating bot..."
-
+function check_env_vars() {
     if [ -z ${KOAKUMA_HOME} ]; then
         echo "\$KOAKUMA_HOME is not defined. It needs to point to the bot's directory."
         exit 1
@@ -20,6 +17,10 @@ function update() {
         echo "\$KOAKUMA_CONNSTR is not defined. It needs to point to the bot's hosting machine."
         exit 1
     fi
+}
+
+function test_conectivity() {
+    check_env_vars
 
     # redirects STDERR to /dev/null to hide ssh error messages
     REMOTE_HOME=$(ssh ${KOAKUMA_CONNSTR} 'source ~/.profile; echo $KOAKUMA_HOME' 2> /dev/null)
@@ -38,6 +39,25 @@ function update() {
 
     # Appending home of the remote koakuma
     TARGET=${KOAKUMA_CONNSTR}:${REMOTE_HOME}
+}
+
+function update_dependencies() {
+    echo "Updating bot dependencies..."
+
+    test_conectivity
+
+    PIP_OUTPUT=$(ssh ${KOAKUMA_CONNSTR} 'source ~/.profile; pip3 install -r $KOAKUMA_HOME/requirements.txt')
+    echo $PIP_OUTPUT
+
+    update
+}
+
+function update() {
+    # Automatically sends updates to the bot. In the future it should also restart the running instance.
+    echo "Updating bot files..."
+
+    test_conectivity
+
     echo "Transferring from ${KOAKUMA_HOME} to ${TARGET}"
 
     rsync -aXv --include=.python-version --exclude=.* --exclude=__pycache__ --exclude=venv --progress ${KOAKUMA_HOME}/ ${TARGET}
@@ -55,6 +75,7 @@ if [ -n "$1" ]; then
     while [ -n "$1" ]; do
         case "$1" in
             -u|--update) update;;
+            -U|--update-dependencies) update_dependencies;;
             -r|--restart) ;;
         esac
         shift

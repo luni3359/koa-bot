9e72eb404e74358e14fa317230deb16a2282164a
luni3359 <lunibackup@outlook.com>
2019-12-06 23:20:52 -0600

    Reuse code

--- converter.py
+++ converter.py
@@ -4,39 +4,29 @@ from itertools import dropwhile
 
 from pint import UnitRegistry
 
+from patterns import NUMBER_PATTERN, UNIT_PATTERN_TUPLE, SPECIAL_UNIT_PATTERN_TUPLE
+
 ureg = UnitRegistry()
 ureg.default_format = '~P.3f'
 Q_ = ureg.Quantity
 
-num = re.compile(r'(\.\d+|\d[,\d]*(?:\.\d+)?)')
-units = [
-    ('feet', re.compile(r' *(\'|foot|feet|ft)(?!\w)')),
-    ('inches', re.compile(r' *("|inch|inches|ins?)(?!\w)')),
-    ('miles', re.compile(r' *(miles?|mi)(?!\w)')),
-    ('pounds', re.compile(r' *(pounds?|lbs?)(?!\w)')),
-    ('meters', re.compile(r' *(meters?|metres?|mt?r?s?)(?!\w)')),
-    ('centimeters', re.compile(r' *(centimeters?|centimetres?|cms?)(?!\w)')),
-    ('kilometers', re.compile(r' *(kilometers?|kilometres?|kms?)(?!\w)')),
-    ('kilograms', re.compile(r' *(kilograms?|kgs?)(?!\w)')),
-]
-foot_inches_special_case = ('footinches', re.compile(r'(\.\d+|\d[,\d]*(?:\.\d+)?)\'(\.\d+|\d[,\d]*(?:\.\d+)?)"?'))
 
 def find_units(s):
     """Get units from a string"""
     results = []
     i = 0
     while i < len(s):
-        special_case_match = foot_inches_special_case[1].match(s, i)
+        special_case_match = SPECIAL_UNIT_PATTERN_TUPLE[1].match(s, i)
         if special_case_match:
-            results.append((foot_inches_special_case[0], float(special_case_match.group(1)), float(special_case_match.group(2))))
+            results.append((SPECIAL_UNIT_PATTERN_TUPLE[0], float(special_case_match.group(1)), float(special_case_match.group(2))))
             i = special_case_match.end()
         else:
-            num_match = num.match(s, i)
+            num_match = NUMBER_PATTERN.match(s, i)
             if num_match:
                 i = num_match.end()
                 match = lambda u: (u[0], u[1].match(s, i))
                 falsey = lambda x: not x[1]
-                unit = next(dropwhile(falsey, map(match, iter(units))), None)
+                unit = next(dropwhile(falsey, map(match, iter(UNIT_PATTERN_TUPLE))), None)
                 if unit:
                     (unit, unit_match) = unit
                     results.append((unit, float(num_match.group(1))))

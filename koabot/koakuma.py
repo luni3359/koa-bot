64af8b948ac1f400b29a14a644e6ae6a4c3209bc
luni3359 <lunibackup@outlook.com>
2020-03-13 16:19:22 -0600

    Fix issue #18

--- koabot/koakuma.py
+++ koabot/koakuma.py
@@ -37,14 +37,24 @@ SOURCE_DIR = os.path.dirname(os.path.realpath(__file__))
 async def roll(ctx, *dice):
     """Rolls one or many dice"""
 
+    if len(dice) < 1:
+        await ctx.send('Please specify what you want to roll.')
+        return
+
     dice_matches = re.findall(DICE_PATTERN, ' '.join(dice))
-    message = '>>> {} rolled the {}.\n'.format(ctx.author.mention, len(dice) > 1 and 'dice' or 'die')
+
+    if not dice_matches:
+        await ctx.send('Sorry, I can\'t do that...')
+        return
+
+    dice_single_or_many = len(dice_matches) > 1 or (dice_matches[0][0] and int(dice_matches[0][0]) > 1)
+    message = '>>> {} rolled the {}.\n'.format(ctx.author.mention, dice_single_or_many and 'dice' or 'die')
     pip_sum = 0
 
-    for die in dice_matches:
-        quantity = die[0] and int(die[0]) or 1
-        pips = die[1] and int(die[1]) or 1
-        bonus_points = die[2] and int(die[2]) or 0
+    for match in dice_matches:
+        quantity = match[0] and int(match[0]) or 1
+        pips = match[1] and int(match[1]) or 1
+        bonus_points = match[2] and int(match[2]) or 0
 
         message += '{} {}-sided {} for a '.format(num2words(quantity).capitalize(), pips, quantity > 1 and 'dice' or 'die')
 

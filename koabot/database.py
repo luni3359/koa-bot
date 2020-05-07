ae9108e9b0ff71cc3605e17ab7827fa437ec9771
luni3359 <lunibackup@outlook.com>
2020-05-07 15:59:18 -0500

    Change table

--- koabot/database.py
+++ koabot/database.py
@@ -1,11 +1,14 @@
 import sqlite3
 
-conn = sqlite3.connect('test.db')
-cursor = conn.cursor()
-# cursor.execute("""CREATE TABLE users (
-#         user_id integer,
-#         discord_id integer
-#     )""")
+conn = sqlite3.connect(':memory:')
+c = conn.cursor()
+
+c.execute("""CREATE TABLE discordUser (
+        userId INTEGER,
+        userName TEXT,
+        userBirthday TEXT
+    )""")
+
 conn.execute("INSERT INTO users VALUES (123, 123)")
 
 conn.commit()

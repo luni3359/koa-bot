362e3f9761c8f5301bfecc988a4f532507fca0fe
luni3359 <lunibackup@outlook.com>
2020-05-06 23:48:12 -0500

    More testing

--- koabot/database.py
+++ koabot/database.py
@@ -2,10 +2,11 @@ import sqlite3
 
 conn = sqlite3.connect('test.db')
 cursor = conn.cursor()
-cursor.execute("""CREATE TABLE users (
-        user_id integer
-        discord_id integer
-    )""")
+# cursor.execute("""CREATE TABLE users (
+#         user_id integer,
+#         discord_id integer
+#     )""")
+conn.execute("INSERT INTO users VALUES (123, 123)")
 
 conn.commit()
 conn.close()

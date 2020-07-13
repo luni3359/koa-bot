d4324eac8bdfe8e52f8485489162a32749449c15
luni3359 <lunibackup@outlook.com>
2020-07-13 00:18:21 -0500

    Testing DiscordUser class

--- db/objects/discord_user.py
+++ db/objects/discord_user.py
@@ -1,3 +1,9 @@
 class DiscordUser:
-    def __init__(self, user_id):
-        self.id = user_id
+    def __init__(self, user_id, user_did, user_name):
+        self.user_id = user_id
+        self.user_did = user_did
+        self.user_name = user_name
+
+    @property
+    def fully_qualified_name(self):
+        return self.user_name + '#1234'

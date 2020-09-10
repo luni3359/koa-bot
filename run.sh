50541bf0cb030e4692fdff92339fdb84113489ba
luni3359 <lunibackup@outlook.com>
2020-09-09 23:09:13 -0500

    Introduce .env file

--- run.sh
+++ run.sh
@@ -10,6 +10,9 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-"$HOME/.config"}
 XDG_CACHE_HOME=${XDG_CACHE_HOME:-"$HOME/.cache"}
 XDG_DATA_HOME=${XDG_DATA_HOME:-"$HOME/.local/share"}
 
+# Loading .env file
+export $(grep -v '^#' .env | xargs)
+
 function show_help() {
     cat << EOF
 Usage: ${0##*/} [OPTION...]

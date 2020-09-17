ee400e7cd48a742f50e28b9fc6ad0b3bad3f439d
luni3359 <lunibackup@outlook.com>
2020-09-16 22:47:32 -0500

    Move source back to test command
    The FFmpegPCMAudio object is used up after being passed over to the bot to be
    played, so it can't be reused.

--- koabot/cogs/music.py
+++ koabot/cogs/music.py
@@ -10,8 +10,6 @@ from koabot.koakuma import SOURCE_DIR
 class Music(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.source = discord.FFmpegPCMAudio(os.path.join(SOURCE_DIR, 'assets', self.bot.testing['vc']['music-file']))
-        self.voice_client = None
 
     @commands.command()
     async def play(self, ctx):
@@ -29,25 +27,37 @@ class Music(commands.Cog):
     async def test(self, ctx):
         """Music test"""
 
-        if not ctx.voice_client:
-            if ctx.guild.voice_channels:
-                for voice_channel in ctx.guild.voice_channels:
-                    try:
-                        voice_client = await voice_channel.connect()
-                        break
-                    except discord.ClientException:
-                        print('Already connected to a voice channel')
-                        continue
-        else:
-            voice_client = ctx.voice_client
+        voice_client = ctx.voice_client
+        author_voice_channel = ctx.author.voice.channel
+        source = discord.FFmpegPCMAudio(os.path.join(SOURCE_DIR, 'assets', self.bot.testing['vc']['music-file']))
 
         if not voice_client:
-            return
+            print('client doesn\'t exist!')
+            if author_voice_channel:
+                print('connecting to author\'s voice channel!')
+                voice_client = await author_voice_channel.connect()
+            else:
+                print('searching for a voice channel...')
+                if not ctx.guild.voice_channels:
+                    print('No voice channels in this server.')
+                    return
 
+                for voice_channel in ctx.guild.voice_channels:
+                    print('connecting to one...!')
+                    voice_client = await voice_channel.connect()
+        else:
+            print('client exists!')
+            if author_voice_channel:
+                print('moving!')
+                await voice_client.move_to(author_voice_channel)
+
+        print('playing music now!')
         if voice_client.is_playing():
+            print('stopping music...')
             voice_client.stop()
 
-        voice_client.play(self.source, after=lambda e: print('done', e))
+        print('playing...')
+        voice_client.play(source, after=lambda e: print('Done playing sound file.', e))
 
 
 def setup(bot: commands.Bot):

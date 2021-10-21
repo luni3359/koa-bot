9a33c1a6ead3886b77df36bb3c6ad1f9ae2f324a
luni3359 <lunibackup@outlook.com>
2021-10-21 03:02:10 -0500

    Use uvloop for a faster asyncio
    Apparently it's only available on Linux.

--- koabot/__main__.py
+++ koabot/__main__.py
@@ -1,7 +1,13 @@
 """__main__.py"""
+import os
 from sys import argv
 
 from koabot.koakuma import start
+
+if os.name != "nt":
+    import uvloop
+
+    uvloop.install()
 
 if __name__ == '__main__':
     start('--debug' in argv)

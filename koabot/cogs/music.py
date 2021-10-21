6695eebe650397f72de67b5788853640a01fc251
luni3359 <lunibackup@outlook.com>
2021-10-20 23:07:15 -0500

    Music: Changed to yt-dlp; Minor changes
    As youtube-dl has basically become debian, we're moving to a newer, much
    more promising fork. Fixes issues with the bot not reporting to users
    its actions.

--- koabot/cogs/music.py
+++ koabot/cogs/music.py
@@ -5,18 +5,18 @@ import random
 import re
 
 import discord
-import youtube_dl
+import yt_dlp as youtube_dl
 from discord.ext import commands
 
 from koabot.koakuma import SOURCE_DIR
 from koabot.patterns import URL_PATTERN
 
 ydl_opts = {
-    'format': 'bestaudio/best',
-    'source_address': '0.0.0.0'
+    'format': "bestaudio/best",
+    'source_address': "0.0.0.0"
 }
 ffmpeg_opts = {
-    'options': '-vn'
+    'options': "-vn"
 }
 ytdl = youtube_dl.YoutubeDL(ydl_opts)
 
@@ -59,47 +59,50 @@ class Music(commands.Cog):
     @commands.command()
     async def join(self, ctx: commands.Context):
         """Joins a voice channel"""
-        voice_client = ctx.voice_client
-        author_voicestate = ctx.author.voice
+        voice_client: discord.VoiceClient = ctx.voice_client
+        author_voicestate: discord.VoiceState = ctx.author.voice
 
         if not voice_client:
             if author_voicestate:
-                voice_client = await author_voicestate.channel.connect()
-                await ctx.send(f'Connected to **"{author_voicestate.channel.name}"** (your voice channel)!')
+                voice_client: discord.VoiceChannel = await author_voicestate.channel.connect()
+                await ctx.send(f"Connected to **\"{author_voicestate.channel.name}\"** (your voice channel)!")
             else:
                 if not ctx.guild.voice_channels:
                     await ctx.send("There's no voice channels in this server...")
                     return
 
                 for voice_channel in ctx.guild.voice_channels:
-                    voice_client = await voice_channel.connect()
-                    await ctx.send(f'Connected to **"{voice_channel.name}"** (the nearest voice channel)!')
+                    voice_client: discord.VoiceClient = await voice_channel.connect()
+                    await ctx.send(f"Connected to **\"{voice_channel.name}\"** (the nearest voice channel)!")
                     break
         else:
             if author_voicestate:
                 if voice_client.channel != author_voicestate.channel:
                     await voice_client.move_to(author_voicestate.channel)
-                    await ctx.send(f'Moved to **"{author_voicestate.channel.name}"** (your voice channel)!')
+                    await ctx.send(f"Moved to **\"{author_voicestate.channel.name}\"** (your voice channel)!")
                 else:
-                    await ctx.send(f'I\'m already in **"{author_voicestate.channel.name}"** (your voice channel)!')
+                    await ctx.send(f"I'm already in **\"{author_voicestate.channel.name}\"** (your voice channel)!")
 
     @commands.command()
     async def leave(self, ctx: commands.Context):
         """Leaves a voice channel"""
-        voice_client = ctx.voice_client
+        voice_client: discord.VoiceClient = ctx.voice_client
 
         if voice_client:
             await voice_client.disconnect()
-            await ctx.send(f'Disconnected from **"{voice_client.channel.name}"**!')
+            await ctx.send(f"Disconnected from **\"{voice_client.channel.name}\"**!")
         else:
-            await ctx.send('No voice channel to disconnect from...')
+            await ctx.send("No voice channel to disconnect from...")
 
     @commands.command()
     async def play(self, ctx: commands.Context, *, search_or_url: str = ""):
         """Plays a track (overrides current track)"""
         # TODO https://stackoverflow.com/a/62360149/7688278
         # Allow links to play from specified timestamps
-        voice_client = ctx.voice_client
+        voice_client: discord.VoiceClient = ctx.voice_client
+
+        if not voice_client:
+            return await ctx.send("I'm not in a voice channel!")
 
         if len(search_or_url) == 0:
             # test a random url while in development
@@ -109,12 +112,17 @@ class Music(commands.Cog):
             url = re.findall(URL_PATTERN, search_or_url)[0]
 
         stream = await YTDLSource.from_url(url, loop=self.bot.loop)
-        voice_client.play(stream, after=lambda e: print('Stream error.') if e else None)
+
+        try:
+            voice_client.play(stream, after=lambda e: print("Stream error.") if e else None)
+        except discord.ClientException as e:
+            print(e)
+            await ctx.send("Can't play!")
 
     @commands.command()
     async def stop(self, ctx: commands.Context):
         """Stops the current track"""
-        voice_client = ctx.voice_client
+        voice_client: discord.VoiceClient = ctx.voice_client
 
         if voice_client and voice_client.is_playing():
             voice_client.stop()
@@ -131,15 +139,15 @@ class Music(commands.Cog):
         await ctx.invoke(self.bot.get_command('join'))
 
         source = discord.FFmpegPCMAudio(os.path.join(SOURCE_DIR, 'assets', self.bot.testing['vc']['music-file']))
-        voice_client = ctx.voice_client
+        voice_client: discord.VoiceClient = ctx.voice_client
 
-        print('playing music now!')
+        print("playing music now!")
         if voice_client.is_playing():
-            print('stopping music...')
+            print("stopping music...")
             voice_client.stop()
 
-        print('playing...')
-        voice_client.play(source, after=lambda e: print('Done playing sound file.', e))
+        print("playing...")
+        voice_client.play(source, after=lambda e: print("Done playing sound file.", e))
 
 
 def setup(bot: commands.Bot):

3faf4af89f2a414313e651088f6fd16d12e298ec
luni3359 <lunibackup@outlook.com>
2021-12-26 22:38:55 -0600

    Reddit previews: Gif support; Act on NSFW status
    Reddit previews should now properly send back a gif preview of gif
    posts. Additionally, NSFW posts are recognized and properly dealt with.
    If there's a NSFW post with multiple images to display in a SFW channel,
    the bot will only send a blurry preview of the first one.

--- koabot/cogs/handler/gallery.py
+++ koabot/cogs/handler/gallery.py
@@ -393,7 +393,7 @@ class Gallery(commands.Cog):
 
                     if (px_views := illust.total_view) > 0:
                         embed.add_field(name='Views', value=f"{px_views:,}")
-                        
+
                     embed.set_author(name=illust.user.name,
                                      url=f"https://www.pixiv.net/users/{illust.user.id}")
 
@@ -624,12 +624,24 @@ class Gallery(commands.Cog):
         header_embed.add_field(name='Comments', value=f"{submission.num_comments:,}")
         footer_text = guide['embed']['footer_text']
 
+        # Determine whether or not to post without thumbnail blur
+        obfuscated_preview = False
+        if not msg.channel.is_nsfw():
+            obfuscated_preview = submission.over_18
+
         embeds = [header_embed]
         # Post has a media gallery
         if hasattr(submission, 'gallery_data'):
             media_count = len(submission.gallery_data['items'])
             ordered_media_data = sorted(submission.gallery_data['items'], key=lambda x: x['id'])
-            for i, item_data in enumerate(ordered_media_data[0:4]):
+            media_type = 'p'        # p = stands for preview?
+            total_to_preview = 4
+
+            if obfuscated_preview:
+                media_type = 'o'    # o = stands for obfuscated?
+                total_to_preview = 1
+
+            for i, item_data in enumerate(ordered_media_data[:total_to_preview]):
                 media_element = submission.media_metadata[item_data['media_id']]
                 if i == 0:
                     embed = header_embed
@@ -639,17 +651,27 @@ class Gallery(commands.Cog):
 
                 if media_element['e'] == "Image":
                     # Fetching the last element from the resolutions list (highest preview-friendly res)
-                    post_preview = media_element['p'][-1]['u']
+                    post_preview = media_element[media_type][-1]['u']
                     embed.set_image(url=post_preview)
 
                 # If we're on the last element
-                if i == len(ordered_media_data[0:4]) - 1:
-                    if media_count > 4:
-                        footer_text = f"{media_count - 4}+ remaining"
-        # Post has only one picture
+                if i == len(ordered_media_data[:total_to_preview]) - 1:
+                    if media_count > total_to_preview:
+                        footer_text = f"{media_count - total_to_preview}+ remaining"
+
+        # Post has only one media element
         elif hasattr(submission, 'preview') and 'images' in submission.preview:
-            post_preview = submission.preview['images'][0]['resolutions'][-1]
-            header_embed.set_image(url=post_preview['url'])
+            preview_root = submission.preview['images'][0]
+
+            # Use blurred-out previews on NSFW posts in SFW channels
+            if obfuscated_preview:
+                preview_root = preview_root['variants']['nsfw']
+            # Show gifs instead if available
+            elif 'variants' in preview_root and 'gif' in preview_root['variants']:
+                preview_root = preview_root['variants']['gif']
+
+            post_preview = preview_root['resolutions'][-1]['url']
+            header_embed.set_image(url=post_preview)
 
         # TODO: Icon urls should be within config guides file
         embeds[-1].set_footer(text=footer_text,

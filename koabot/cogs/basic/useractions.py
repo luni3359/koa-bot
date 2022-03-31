458d7189925ad711328013fe5ade39fa156123e6
luni3359 <lunibackup@outlook.com>
2022-03-31 14:03:59 -0600

    Add new command: inspire
    It shows inspirational quotes, I think it's pretty positive! Got the
    idea from a video on youtube

--- koabot/cogs/basic/useractions.py
+++ koabot/cogs/basic/useractions.py
@@ -2,6 +2,7 @@
 import discord
 from discord.ext import commands
 
+import koabot.core.net as net_core
 from koabot.kbot import KBot
 
 
@@ -30,6 +31,17 @@ class UserActions(commands.Cog):
             embeds.append(embed)
 
         await ctx.send(embeds=embeds)
+
+    @commands.command()
+    async def inspire(self, ctx: commands.Context):
+        """Get a random inspirational quote"""
+        # Thanks for the idea freecodecamp!
+
+        if not (api_response := (await net_core.http_request("https://zenquotes.io/api/random", json=True)).json):
+            return await ctx.send("I cannot channel those energies at the moment... Please try again later")
+
+        quote, author, _ = api_response[0].values()
+        await ctx.send(f">>> \"{quote}\"\nー *{author}*")
 
 
 async def setup(bot: KBot):
